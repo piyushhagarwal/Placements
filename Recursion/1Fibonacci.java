cd5767c2db22be2b1bb6b569773bb48b86bdea9e
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-10-10 22:28:43 +0530

    Recusrsion Q1

---
+++ Recursion/1Fibonacci.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+class Fibonacci {
+
+    // Normal method
+    public int fib1(int n) {
+        int f0 = 0;
+        int f1 = 1;
+        int f2 = 1;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        for (int i = 2; i <= n; i++) {
+            f2 = f1 + f0;
+            f0 = f1;
+            f1 = f2;
+        }
+        return f2;
+    }
+
+    // Recursive method
+    public int fib2(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return fib2(n - 1) + fib2(n - 2);
+    }
+
+}
