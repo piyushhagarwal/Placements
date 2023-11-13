609d1cb0b5234df53d9866544bd49c559f9434f0
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-13 09:01:52 +0530

    Basic operations on SLL

---
+++ DSA/LinkedLists/2SinglyLinkedList.java
@@ -0,0 +1,95 @@
+package LinkedLists;
+
+class Node {
+    public int data;
+    public Node next;
+
+    Node() {
+        data = -1;
+        next = null;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SSL {
+    public static Node head = new Node();
+
+    public static void InsertAtBeginning(int data) {
+        // If the LinkedList is Empty
+        if (head.data == -1) {
+            head.data = data;
+        } else {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public static void InsertAtEnd(int data) {
+        // If the LinkedList is Empty
+        if (head.data == -1) {
+            head.data = data;
+        } else {
+            Node newNode = new Node(data);
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public static void DeleteFromBeginning() {
+        // If the LinkedList is Empty
+        if (head.data == -1) {
+            System.out.println("Linked List is empty");
+        } else {
+            Node tempHead = head.next;
+            head.next = null;
+            head = tempHead;
+        }
+    }
+
+    public static void DeleteFromEnd() {
+        // If the LinkedList is Empty
+        if (head.data == -1) {
+            System.out.println("Linked List is empty");
+        } else {
+            Node prev = null;
+            Node current = head;
+            while (current.next != null) {
+                prev = current;
+                current = current.next;
+            }
+            prev.next = null;
+        }
+    }
+
+    public static void printLL(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        // InsertAtBeginning(1);
+        // InsertAtBeginning(2);
+        // InsertAtBeginning(3);
+        // InsertAtBeginning(4);
+        // printLL(head);
+
+        InsertAtEnd(1);
+        InsertAtEnd(2);
+        InsertAtEnd(3);
+        InsertAtEnd(4);
+        DeleteFromEnd();
+        printLL(head);
+    }
+}
