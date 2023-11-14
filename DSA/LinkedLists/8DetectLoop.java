8847c06594856f1803c490516a5a8b95886680c4
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-14 13:30:53 +0530

    Added ListNode class

--- DSA/LinkedLists/8DetectLoop.java
+++ DSA/LinkedLists/8DetectLoop.java
@@ -26,6 +26,24 @@ package LinkedLists;
 
 import java.util.ArrayList;
 
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class DetectLoop {
     // Naive Approach
     public boolean hasCycleNaive(ListNode head) {
