a390430d789a5e5bde14ca4877a2f12bde8c5f8b
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-14 12:17:02 +0530

    Added recursion solution

--- DSA/LinkedLists/7ReverseLL.java
+++ DSA/LinkedLists/7ReverseLL.java
@@ -40,4 +40,16 @@ class ReverseLL {
 
         return currNode;
     }
+
+    public ListNode Recursive(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = Recursive(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
 }
