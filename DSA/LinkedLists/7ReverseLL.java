5bcdbf1beed12f17989e15c6c22272e3ec7adc8e
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-14 11:20:33 +0530

    Reverse LL

---
+++ DSA/LinkedLists/7ReverseLL.java
@@ -0,0 +1,43 @@
+package LinkedLists;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return head;
+        }
+
+        ListNode prevNode = null;
+        ListNode currNode = head;
+        ListNode nextNode = currNode.next;
+
+        while (nextNode != null) {
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+            nextNode = nextNode.next;
+        }
+
+        currNode.next = prevNode;
+
+        return currNode;
+    }
+}
