fb9ea41af686c01e8eeb83f80664e9dbebff09bd
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-24 10:15:08 +0530

    Added brute force solution

--- DSA/SearchingSorting/6CountInversion.java
+++ DSA/SearchingSorting/6CountInversion.java
@@ -1,10 +1,39 @@
-package SearchingSorting;
+// Inversion Count for an array indicates – how far (or close) the array is from being sorted. If the array is already sorted, then the inversion count is 0, but if the array is sorted in reverse order, the inversion count is the maximum. 
+
+// Given an array arr[]. The task is to find the inversion count of arr[]. Where two elements arr[i] and arr[j] form an inversion if a[i] > a[j] and i < j.
+
+// Examples: 
+
+// Input: arr[] = {8, 4, 2, 1}
+// Output: 6
+// Explanation: Given array has six inversions: (8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+// Input: arr[] = {1, 20, 6, 4, 5}
+// Output: 5
+// Explanation: Given array has five inversions: (20, 6), (20, 4), (20, 5), (6, 4), (6, 5). 
 
 //Problem link: https://www.geeksforgeeks.org/inversion-count-in-array-using-merge-sort/
 //Problem solution video: https://www.youtube.com/watch?v=AseUmwVNaoY
 
+package SearchingSorting;
+
 class Solution {
 
+    // Bruteforce approach
+    public static int numberOfInversions1(int[] a, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    // Time complexity: O(n^2)
+
+    // Optimized approach
     static int count = 0;
 
     public static void solve(int[] arr, int start, int end) {
