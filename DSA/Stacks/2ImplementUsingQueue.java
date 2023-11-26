abebf794a8f7fe292957ea20d56be0c1ed1b8e29
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-11-26 21:32:43 +0530

    Implemented stack using queue

---
+++ DSA/Stacks/2ImplementUsingQueue.java
@@ -0,0 +1,52 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+
+// Link : https://leetcode.com/problems/implement-stack-using-queues/
+
+package Stacks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.add(queue.remove());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
