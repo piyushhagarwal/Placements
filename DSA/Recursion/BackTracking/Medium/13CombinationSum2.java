807c772c9ed58d8b4f38f7d5bb456366aee7f82a
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2023-12-16 12:31:32 +0530

    Added brute force method

--- DSA/Recursion/BackTracking/Medium/13CombinationSum2.java
+++ DSA/Recursion/BackTracking/Medium/13CombinationSum2.java
@@ -30,11 +30,46 @@ package Recursion.BackTracking.Medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 class Solution {
 
-    // Helper function for backtracking
+    // Bruteforce approach
+    public void helper(int index, int[] candidates, List<Integer> subset, int target, HashSet<List<Integer>> ans) {
+        // Base Case: If the target is reached
+        if (target == 0) {
+            ans.add(new ArrayList<>(subset)); // Add the current subset to the set (avoiding duplicates)
+            return;
+        }
+
+        // Base Cases to stop recursion
+        if (target < 0 || index >= candidates.length) {
+            return;
+        }
+
+        // Exclude current element and move to the next index
+        helper(index + 1, candidates, subset, target, ans);
+
+        // Include current element in the subset
+        int element = candidates[index];
+        subset.add(element);
+        // Recursively call the helper function with updated target
+        helper(index + 1, candidates, subset, target - element, ans);
+        subset.remove(subset.size() - 1); // Backtrack: Remove the last element for backtracking
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int index = 0;
+        Arrays.sort(candidates); // Sort the candidates to handle duplicates and optimize the search
+        List<Integer> subset = new ArrayList<>();
+        HashSet<List<Integer>> ans = new HashSet<>(); // Using a HashSet to automatically handle duplicates
+        helper(index, candidates, subset, target, ans);
+        List<List<Integer>> ans1 = new ArrayList<>(ans); // Convert HashSet to List for the final result
+        return ans1;
+    }
+
+    // Optimized approach
     public static void helper(int[] candidates, int index, List<Integer> subset, List<List<Integer>> result,
             int target) {
         // If the target is achieved, add the current subset to the result
@@ -65,7 +100,7 @@ class Solution {
     }
 
     // Main function to find combination sums
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    public List<List<Integer>> combinationSumTwo(int[] candidates, int target) {
         // List to store the final result
         List<List<Integer>> result = new ArrayList<>();
         // Sort the candidates to handle duplicates efficiently
