26c5bba46d2278c7e1ba7fbb43c625ccaf890a02
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-01-04 12:34:47 +0530

    Add solution for deleting a node in a BST

---
+++ DSA/BinarySearchTrees/4.DeleteNode.java
@@ -0,0 +1,111 @@
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+// Basically, the deletion can be divided into two stages:
+
+// Search for a node to remove.
+// If the node is found, delete the node.
+
+// Example 1:
+
+// Input: root = [5,3,6,2,4,null,7], key = 3
+// Output: [5,4,6,2,null,null,7]
+// Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+// One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+// Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,7], key = 0
+// Output: [5,3,6,2,4,null,7]
+// Explanation: The tree does not contain a node with value = 0.
+// Example 3:
+
+// Input: root = [], key = 0
+// Output: []
+
+// Link : https://leetcode.com/problems/delete-node-in-a-bst/
+// Solution : https://www.youtube.com/watch?v=UeRUKRJvPa4&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=75&t=2545s
+
+package BinarySearchTrees;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int getMinVal(TreeNode root) {
+        TreeNode curr = root;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr.val;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            // Case 1: If the node is leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // Case 2: If node has right child
+            else if (root.left == null && root.right != null) {
+                return root.right;
+            }
+
+            // Case 3: If node has left child
+            else if (root.left != null && root.right == null) {
+                return root.left;
+            }
+
+            // Case 4: If node has both right and left child
+            else {
+                int minVal = getMinVal(root.right);
+                root.val = minVal;
+                root.right = deleteNode(root.right, minVal);
+            }
+        } else {
+            if (key > root.val) {
+                root.right = deleteNode(root.right, key);
+            } else {
+                root.left = deleteNode(root.left, key);
+            }
+        }
+
+        return root;
+
+    }
+}
