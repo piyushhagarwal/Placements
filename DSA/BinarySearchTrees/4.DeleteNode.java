27a747bac3391ebcf1dbe243dc5da6cb916536c6
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-01-05 10:31:59 +0530

    Remove unnecessary comment and improve code readability

--- DSA/BinarySearchTrees/4.DeleteNode.java
+++ DSA/BinarySearchTrees/4.DeleteNode.java
@@ -28,21 +28,6 @@
 
 package BinarySearchTrees;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
 class Solution {
     class TreeNode {
         int val;
