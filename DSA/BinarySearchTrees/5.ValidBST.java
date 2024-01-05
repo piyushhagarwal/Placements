09479791e9d4fb5d3e81b4dd786d6db6dc0f8af7
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-01-05 10:30:35 +0530

    Add solution for validating a binary search tree

---
+++ DSA/BinarySearchTrees/5.ValidBST.java
@@ -0,0 +1,70 @@
+// Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+// A valid BST is defined as follows:
+
+// The left 
+// subtree
+//  of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+
+// Example 1:
+
+// Input: root = [2,1,3]
+// Output: true
+// Example 2:
+
+// Input: root = [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4.
+
+// Link : https://leetcode.com/problems/validate-binary-search-tree/
+
+package BinarySearchTrees;
+
+import java.util.ArrayList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public void inorder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list); // Inorder traversal of BST gives sorted array
+
+        // Check if the array is sorted
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
