c6965f8dea21eb8eca6aaa2c5e965c011f3201be
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-06-01 17:56:54 +0530

    added method to find leaf nodes with the help of queue

--- DSA/BinaryTrees/6.NumberOfLeafNodes.java
+++ DSA/BinaryTrees/6.NumberOfLeafNodes.java
@@ -1,6 +1,9 @@
 // Link : https://www.codingninjas.com/studio/problems/count-leaf-nodes_893055
 package BinaryTrees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // Following is the Binary Tree Node class structure.
 class BinaryTreeNode<T> {
     T data;
@@ -26,4 +29,37 @@ class NumberOfLeafNodes {
 
         return noOfLeafNodes(root.left) + noOfLeafNodes(root.right);
     }
+
+    // By using queue
+    public static int noOfLeafNodes2(BinaryTreeNode<Integer> root) {
+        // Write your code here.
+
+        if (root == null) {
+            return 0;
+        }
+
+        int count = 0;
+
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            BinaryTreeNode<Integer> poppedElement = queue.poll();
+
+            if (poppedElement.left == null && poppedElement.right == null) {
+                count++;
+            }
+
+            if (poppedElement.left != null) {
+                queue.add(poppedElement.left);
+            }
+
+            if (poppedElement.right != null) {
+                queue.add(poppedElement.right);
+            }
+        }
+
+        return count;
+    }
+
 }
