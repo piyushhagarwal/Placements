5f32d1abdc467d94ba551001e822146e0bf90053
piyushhagarwal <91668990+piyushhagarwal@users.noreply.github.com>
2024-07-09 08:39:58 +0530

    Added solution video, Algorithm and leetcode link

--- DSA/Arrays/4Sort012.java
+++ DSA/Arrays/4Sort012.java
@@ -1,7 +1,22 @@
 // Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
 // In O(n) time complexity
 // It is known as Dutch national flag problem(https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/)
+
+// Video Explanation: https://www.youtube.com/watch?v=tp8JIuCXBaU (Great explanation)
+// leetcode: https://leetcode.com/problems/sort-colors/description/
 package Arrays;
+
+// Algorithm:
+// The idea is to use three pointers low, mid, and high.
+// low - 0 to low-1 contains 0s
+// mid - low to mid-1 contains 1s
+// high - high+1 to n-1 contains 2s
+// We assume that mid to high contains unknown elements that are unsorted.
+// We will iterate over the array and check the value at mid.
+// If the value at mid is 0, then we will swap the value at low and mid and increment low and mid.
+// If the value at mid is 1, then we will increment mid.
+// If the value at mid is 2, then we will swap the value at mid and high and decrement high.
+// We will continue this process until mid is less than or equal to high.
 
 class Sort012 {
     static void sort012(int arr[], int n) {
